8925764ace8b9c0a2db43b0f370c116c4c13c8a5
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2020-11-27 04:08:27 +0900

    fix function path

--- .fet/function/functions/help.zsh
+++ .fet/function/functions/help.zsh
@@ -1,3 +1,8 @@
 #!/bin/zsh
 # echo '0' >| ~/.fet/.status/.functions_help.status
-richcat ~/.fet/ReadMe.md -w 0.9 | fzf --height=80% --ansi --bind "alt-h:abort,alt-j:down,alt-k:up,alt-l:abort,alt-c:abort,ESC:abort"
+which richcat
+if [ "`which richcat`" = "richcat not found" ]; then
+  cat ~/.fet/ReadMe.md | fzf --height=80% --ansi --bind "alt-h:abort,alt-j:down,alt-k:up,alt-l:abort,alt-c:abort,ESC:abort"
+else
+  richcat ~/.fet/ReadMe.md -w 0.9 | fzf --height=80% --ansi --bind "alt-h:abort,alt-j:down,alt-k:up,alt-l:abort,alt-c:abort,ESC:abort"
+fi
