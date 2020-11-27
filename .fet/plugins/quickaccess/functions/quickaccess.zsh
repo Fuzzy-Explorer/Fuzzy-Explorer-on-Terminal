f8cb1a495c5a3a69fdaa92bc8db5e2e017d1d3ef
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2020-11-27 13:34:10 +0900

    fix delete QA bug

--- .fet/plugins/quickaccess/functions/quickaccess.zsh
+++ .fet/plugins/quickaccess/functions/quickaccess.zsh
@@ -28,10 +28,12 @@ do
   _fet_quickaccess_name=$(echo $_fet_quickaccess_name | awk -F"\t" '{print $2}' )
   local _fet_var_delete_quickaccess=$(cat ~/.fet/.status/.quickaccess_delete_quickaccess.status)
   if [ $_fet_var_delete_quickaccess = '1' ]; then
-    echo "$_fet_colorcode_yellow""Do you want to delete quickaccess='$_fet_quickaccess_name'?$_fet_colorcode_end ($_fet_colorcode_green"'y'"$_fet_colorcode_end"'/'"$_fet_colorcode_red"'n'"$_fet_colorcode_end"')'
+    echo "$_fet_colorcode_yellow""Do you want to delete quickaccess""$_fet_colorcode_end""='$_fet_quickaccess_name'?$_fet_colorcode_end ($_fet_colorcode_green"'y'"$_fet_colorcode_end"'/'"$_fet_colorcode_red"'n'"$_fet_colorcode_end"')'
     read -r;
     if [ "$REPLY" = "y" ]; then
       . ~/.fet/plugins/quickaccess/functions/delete_quickaccess.zsh
+    else
+      echo '0' >| ~/.fet/.status/.quickaccess_delete_quickaccess.status
     fi
   else;
     if [ -n $_fet_quickaccess_name ]; then
@@ -43,10 +45,12 @@ do
         break
       else;
         echo "no such file or directory. ($_fet_var_quickaccess_pathes[$_fet_quickaccess_name])"
-        echo "$_fet_colorcode_yellow""Do you want to delete quickaccess='$_fet_quickaccess_name'?$_fet_colorcode_end ($_fet_colorcode_green"'y'"$_fet_colorcode_end"'/'"$_fet_colorcode_red"'n'"$_fet_colorcode_end"')'
+        echo "$_fet_colorcode_yellow""Do you want to delete quickaccess""$_fet_colorcode_end""='$_fet_quickaccess_name'?$_fet_colorcode_end ($_fet_colorcode_green"'y'"$_fet_colorcode_end"'/'"$_fet_colorcode_red"'n'"$_fet_colorcode_end"')'
         read -r;
         if [ "$REPLY" = "y" ]; then
           . ~/.fet/plugins/quickaccess/functions/delete_quickaccess.zsh
+        else
+          echo '0' >| ~/.fet/.status/.quickaccess_delete_quickaccess.status
         fi
       fi
     fi
