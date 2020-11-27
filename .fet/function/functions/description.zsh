2ec62803336973f7f155ab161bfec8c4eabaa74d
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2020-11-27 09:14:20 +0900

    description DESCRIPTION -> Dir/File

--- .fet/function/functions/description.zsh
+++ .fet/function/functions/description.zsh
@@ -1,15 +1,20 @@
 #!/bin/zsh
 if (test -d $_fet_path_selected_path); then
-  vim "$_fet_path_selected_path/.description.lsi"
+  if [ -f "$_fet_path_selected_path/.description.lsi" ]; then
+    vim "$_fet_path_selected_path/.description.lsi"
+  else
+    echo "Dir" >| "$_fet_path_selected_path/.description.lsi"
+    vim "$_fet_path_selected_path/.description.lsi"
+  fi
 else;
   if [ -e "$PWD/.file_description.lsi" ]; then
     local exists_file_desc=$(cat "$PWD/.file_description.lsi" | grep "^\\\\/$_fet_path_selected_path")
     if [ -n "$exists_file_desc" ]; then
     else;
-      mkdiri -f "$PWD/$_fet_path_selected_path" "DESCRIPTION"
+      mkdiri -f "$PWD/$_fet_path_selected_path" "File"
     fi
   else;
-    mkdiri -f "$PWD/$_fet_path_selected_path" "DESCRIPTION"
+    mkdiri -f "$PWD/$_fet_path_selected_path" "File"
   fi
   vim "$PWD/.file_description.lsi" -c /$_fet_path_selected_path
 fi
