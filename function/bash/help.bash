527fd14d86ef41fd63f1c840ac3f83c951e3a982
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2020-11-28 05:50:07 +0900

    ReadMe.md -> help.md

--- function/bash/help.bash
+++ function/bash/help.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ "`which richcat`" = "richcat not found" ]; then
-  cat ~/.fet/ReadMe.md | fzf --height=80% --ansi --info='inline' --layout=reverse --border --cycle --bind "alt-h:abort,alt-j:down,alt-k:up,alt-l:abort,alt-c:abort,ESC:abort,left:abort,down:down,up:up,right:abort"
+if [ -n "`which richcat`" ]; then
+  richcat ~/.fet/help.md -w 0.9 | fzf --height=80% --ansi --bind "alt-h:abort,alt-j:down,alt-k:up,alt-l:abort,alt-c:abort,ESC:abort"
 else
-  richcat ~/.fet/ReadMe.md -w 0.9 | fzf --height=80% --ansi --bind "alt-h:abort,alt-j:down,alt-k:up,alt-l:abort,alt-c:abort,ESC:abort"
+  cat ~/.fet/help.md | fzf --height=80% --ansi --info='inline' --layout=reverse --border --cycle --bind "alt-h:abort,alt-j:down,alt-k:up,alt-l:abort,alt-c:abort,ESC:abort,left:abort,down:down,up:up,right:abort"
 fi
